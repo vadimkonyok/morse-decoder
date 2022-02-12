18005fd6555e078badeba5f820d7f196d3b98ad6
vadimkonyok <vadim.konyok@mail.ru>
2022-02-12 11:45:00 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        array.push(expr.slice(i, i + 10))
+    }
+    const removeZeros = str => {
+        if (str.indexOf(1) === -1) { return ' '}
+        return str.slice(str.indexOf(1))
+    }
+    const replaceWithDots = str => {
+        if (str === ' ') {
+            return ' '
+        }
+        for (let i = 0; i < str.length; i++) {
+            str = str.replace('11', '-')
+            str = str.replace('10', '.')
+        }
+        return str;
+    }
+    const decodedString = array
+        .map(item => removeZeros(item))
+        .map(item => replaceWithDots(item))
+        .map(item => MORSE_TABLE[item]||' ')
+        .join('')
+    
+    return decodedString;
 }
 
 module.exports = {
